c873f97b8cd787a55fdb4e7b8198027dbf12d4cd
MisterStank <chakkritchongkraichak@gmail.com>
2023-10-29 15:09:12 +0700

    add folders

---
+++ bodydough-app/src/components/Intro.tsx
@@ -0,0 +1,18 @@
+
+
+function Intro() {
+  return (
+    <div>
+        <div className='relative top-20 z-20 text-center text-xl'>
+            <h1>Body dough</h1>
+        </div>
+        <button className='bg-white text-green-700 border border-green-700
+            font-semibold py-2 px-2 m-2 rounded z-30 absolute bottom-0 left-11
+            hover:bg-green-700 hover:text-white hover:border-transparent'>
+                Start
+        </button>
+    </div>
+  )
+}
+
+export default Intro
