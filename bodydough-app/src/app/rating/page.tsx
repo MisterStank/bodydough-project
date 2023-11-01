12df5dd623578198d9df31601ed4dc362493a331
naphattar <naphatchalermwai@gmail.com>
2023-11-01 13:40:10 +0700

    add rating page

--- bodydough-app/src/app/rating/page.tsx
+++ bodydough-app/src/app/rating/page.tsx
@@ -1,6 +1,10 @@
+
 import Image from "next/image";
-import ReactStars from "react-stars";
+
+
+
 export default function RatingPage(){
+   
     return(
         <div className='w-screen h-screen flex divide-x items-center justify-center h-screen'>
             <Image
@@ -10,15 +14,10 @@ export default function RatingPage(){
                 objectFit='cover'
             />
             <div className='w-60% mg-4 pd-20 relative flex flex-col font-sriracha items-center justify-center rounded-lg border-4 border-gray-500'>
-                <ReactStars 
-                count={5} 
-                size={24} 
-                color2={'#ffd700'} 
-                /> 
-                <p className='md:text-xl lg:text-3xl xl:text-4xl mb-2 mt-5'>ความพึงพอใจในการใช้เว็บไซต์</p>
+                <p className='md:text-xl lg:text-3xl xl:text-4xl mb-5 mt-5'>ความพึงพอใจในการใช้เว็บไซต์</p>
                     <button className='bg-yellow-500 text-black border-4 border-black 
                     font-medium text-xl font-prompt py-2 px-10 rounded-full  hover:bg-white
-                    hover:text-yellow-600 hover:border-yellow-600 mb-5' >
+                    hover:text-yellow-600 hover:border-yellow-600 mb-5 mt-5' >
                         SUBMIT
                     </button>
             </div>
