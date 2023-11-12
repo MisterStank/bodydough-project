a08d1eda082aa56a15d4e561b1d21bc3b93cd492
MisterStank <chakkritchongkraichak@gmail.com>
2023-11-13 01:52:37 +0700

    Update page.tsx

--- bodydough-app/src/app/info/page.tsx
+++ bodydough-app/src/app/info/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { useRouter} from "next/navigation";
 import { calculateBmi, calculateResult } from "../utills/calculateresult";
+import { LinearProgress } from "@mui/material";
 
 function Infopage() {
     const [name,setName] = useState<string>('');
@@ -12,6 +13,7 @@ function Infopage() {
     const [height,setHeight] = useState<number>(0);
     const [waist,setWaist] = useState<number>(0);
     const [hip,setHip] = useState<number>(0);
+    const [loading, setLoading] = useState<boolean>(false); 
 
 
     const router = useRouter();
@@ -89,10 +91,20 @@ function Infopage() {
           } catch (error) {
             console.error(error);
           }
+          finally {
+            setLoading(false); // Set loading back to false when the operation is done
+        }
     };
 
     return (
         <div className='w-screen h-screen flex divide-x items-center justify-center'>
+            {loading ? ( // Conditionally render loading screen
+                <div className="loading-screen">
+                    <h1 className="m-5 font-sriracha text-3xl">Loading your result...</h1>
+                    <LinearProgress color="inherit"/>
+                </div>
+            ) : (
+            <>
             <Image
                 src={'/Infopage.jpg'}
                 alt='cover'
@@ -159,6 +171,8 @@ function Infopage() {
                         ENTER
                     </button>
             </div>
+            </>
+            )}
         </div>
     )
 }
