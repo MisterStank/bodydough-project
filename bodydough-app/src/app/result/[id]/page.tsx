1dc3a9ba4d6e27de478fb8a40574b9a5a3a4dec3
MisterStank <chakkritchongkraichak@gmail.com>
2023-11-13 01:31:14 +0700

    Update loading page

--- bodydough-app/src/app/result/[id]/page.tsx
+++ bodydough-app/src/app/result/[id]/page.tsx
@@ -2,8 +2,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useSearchParams } from 'next/navigation'
+import { useState,useEffect } from "react";
+import { LinearProgress } from "@mui/material";
 
-export default async function Page({ params }: { params: { id: number } } ) {
+export default function Page({ params }: { params: { id: number } } ) {
    const searchParams = useSearchParams()
    const id = params.id;
    const imageIDs = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,7,8,9,7,8,9,16,17,18,16,17,18];
@@ -106,54 +108,63 @@ export default async function Page({ params }: { params: { id: number } } ) {
     "ไม่สมส่วน",
     "ไม่สมส่วน",
    ];
-   
-   await new Promise(resolve => setTimeout(resolve,1000))
+   const [loading, setLoading] = useState(true);
+   useEffect(() => {
+    // Simulate a delay to fetch data (you can replace this with actual data fetching logic)
+    const fetchData = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulating a 2-second delay
+      setLoading(false); // Set loading to false once data is ready
+    };
 
-   return (
-   <div>
-    <div className='flex flex-col items-center h-5/6'>
-      <Image
-        src={'/bgresult.png'}
-        alt='cover'
-        fill={true}
-        objectFit='cover'
-      />
-      <div className='my-5 mx-10 grid grid-cols-1 sm:grid-cols-2 gap-1 z-50 overflow-y-auto overflow-x-hidden scroll-smooth
-        scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-300 scrollbar- scrollbar-thumb-rounded-full scrollbar-track-rounded-full'>
-        <div className="flex-none w-full h-[300px] sm:h-[500px] relative">
-            <Image src={`/images/result/${imageIDs[id-1]}.png`}
-               alt='card'
-               fill={true}
-               className='object-contain rounded-lg' />
-         </div>
-         <div className="m-5 flex w-[90%] sm:w-[80%] max-h-[400px] sm:max-h-[600px] relative flex-col item-center">
-                    <div className='p-5 w-[100%] max-h-[400px] sm:max-h-[600px] text-[min(4vw,1.5rem)] text-gray-700 rounded-lg 
-                        flex font-ktd bg-white shadow-md z-50 '
-                        style={{ backgroundImage: `url('/images/19.png')`, backgroundSize: '100% 100%' }}>
-                        <div className="m-5 leading-relaxed
-                            overflow-y-auto overflow-x-hidden scroll-smooth
-                            scrollbar-thin scrollbar-thumb-transparent scrollbar-track-transparent scrollbar- scrollbar-thumb-rounded-full scrollbar-track-rounded-full">
-                            <li>ดัชนีมวลกาย(BMI) = <b>{bmi}</b> <br/>
-                                {resultFromBmi[id-1]} 
-                            </li>
-                            <li>
-                            {resultFromWhr[id-1]}<br/>
-                            ร่างกาย{finalResult[id-1]} และ{resultFromWhr[id-1]}
-                            </li> 
-                        </div>
-                    </div>
+    fetchData();
+  }, []); // Empty dependency array ensures the effect runs only once on component mount
+
+  return (
+    <div>
+      {loading ? (
+        // Render loading screen while data is being fetched
+        <div className="loading-screen">
+          <h1 className="m-5 font-sriracha text-3xl">Loading your result...</h1>
+          <LinearProgress color="inherit"/>
+        </div>
+      ) : (
+        // Render the actual content once data is ready
+        <div className='flex flex-col items-center h-5/6'>
+          <Image src={'/bgresult.png'} alt='cover' fill={true} objectFit='cover' />
+          <div className='my-5 mx-10 grid grid-cols-1 sm:grid-cols-2 gap-1 z-50 overflow-y-auto overflow-x-hidden scroll-smooth
+            scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-300 scrollbar- scrollbar-thumb-rounded-full scrollbar-track-rounded-full'>
+            <div className="flex-none w-full h-[300px] sm:h-[500px] relative">
+              <Image src={`/images/result/${imageIDs[id - 1]}.png`} alt='card' fill={true} className='object-contain rounded-lg' />
+            </div>
+            <div className="m-5 flex w-[90%] sm:w-[80%] max-h-[400px] sm:max-h-[600px] relative flex-col item-center">
+              <div className='p-5 w-[100%] max-h-[400px] sm:max-h-[600px] text-[min(4vw,1.5rem)] text-gray-700 rounded-lg 
+                  flex font-ktd bg-white shadow-md z-50 '
+                  style={{ backgroundImage: `url('/images/19.png')`, backgroundSize: '100% 100%' }}>
+                <div className="m-5 leading-relaxed
+                      overflow-y-auto overflow-x-hidden scroll-smooth
+                      scrollbar-thin scrollbar-thumb-transparent scrollbar-track-transparent scrollbar- scrollbar-thumb-rounded-full scrollbar-track-rounded-full">
+                  <li>ดัชนีมวลกาย(BMI) = <b>{bmi}</b> <br />
+                    {resultFromBmi[id - 1]}
+                  </li>
+                  <li>
+                    {resultFromWhr[id - 1]}<br />
+                    ร่างกาย{finalResult[id - 1]} และ{resultFromWhr[id - 1]}
+                  </li>
                 </div>
-      </div>
-
-      <div className='z-50'>
-        <Link href={'/bmi-rating'}>
-          <button className='bg-yellow-500 text-black border-4 border-black 
-          font-medium text-xl font-prompt py-2 px-10 rounded-full  hover:bg-white
-           hover:text-yellow-600 hover:border-yellow-600'>
-            GO
-          </button>
-        </Link>
-      </div>
+              </div>
+            </div>
+          </div>
+          <div className='z-50'>
+            <Link href={'/bmi-rating'}>
+              <button className='bg-yellow-500 text-black border-4 border-black 
+                font-medium text-xl font-prompt py-2 px-10 rounded-full  hover:bg-white
+                hover:text-yellow-600 hover:border-yellow-600'>
+                GO
+              </button>
+            </Link>
+          </div>
+        </div>
+      )}
     </div>
-   </div>);
+  );
 }
